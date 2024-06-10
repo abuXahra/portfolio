2f6c9939a88244ffde9e7ea84b8a8c2448d472ed
Abdulmumin Isah <abdulmuminisah@yahoo.com>
2024-06-10 12:23:06 +0100

    updated

--- src/data/mern.js
+++ src/data/mern.js
@@ -1,15 +1,18 @@
-
-import news from '../images/mern/news.PNG'
-
-
-
+import news from "../images/mern/news.PNG";
 
 export const mern = [
-    {
-        title: 'News Blog',
-        description: "It is a news blog Web App",
-        imgUrl: news,
-        siteUrl: "https://newsblog-244u.onrender.com/",
-        status: "Online",
-    },
-]
+  {
+    title: "News Blog",
+    description: "It is a news blog Web App",
+    imgUrl: news,
+    siteUrl: "https://newsblog-244u.onrender.com/",
+    status: "Online",
+  },
+  {
+    title: "Manzo Travels Website",
+    description: "Travel and Tour Agency Website",
+    imgUrl: news,
+    siteUrl: "https://manzotravelandtours.netlify.app/",
+    status: "Online",
+  },
+];
