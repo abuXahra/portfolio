65c1665c4666c83e4e580d667f65d1a942125b10
Abdulmumin Isah <abdulmuminisah@yahoo.com>
2024-06-28 11:59:52 +0100

    updated

--- src/data/voteCategorJson.js
+++ src/data/voteCategorJson.js
@@ -1,102 +1,113 @@
-import { FaArrowRight, FaNode, FaNodeJs } from 'react-icons/fa';
-import { FaMicrosoft, FaFilePowerpoint, FaGithub, FaHtml5, FaReact, FaWordpress, FaCss3 } from 'react-icons/fa'
-import { BsFileWord, BsGit } from 'react-icons/bs'
-import { RiLoader3Line, RiFlutterFill } from 'react-icons/ri'
-import { DiJavascript1, DiMongodb } from 'react-icons/di'
-import { SiAdobefonts, SiDart, SiHuawei } from 'react-icons/si'
-import { GiParachute, GiWireframeGlobe } from 'react-icons/gi'
-import { FiFigma, } from 'react-icons/fi'
+import { FaArrowRight, FaNode, FaNodeJs } from "react-icons/fa";
+import {
+  FaMicrosoft,
+  FaFilePowerpoint,
+  FaGithub,
+  FaHtml5,
+  FaReact,
+  FaWordpress,
+  FaCss3,
+} from "react-icons/fa";
+import { BsFileWord, BsGit } from "react-icons/bs";
+import { RiLoader3Line, RiFlutterFill } from "react-icons/ri";
+import { DiJavascript1, DiMongodb } from "react-icons/di";
+import { SiAdobefonts, SiDart, SiHuawei } from "react-icons/si";
+import { GiParachute, GiWireframeGlobe } from "react-icons/gi";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FiFigma } from "react-icons/fi";
 export const voteCategories = [
-    {
-        voteIcon: <FaNodeJs />,
-        title: "Node Js",
-        arrowIcon: <RiLoader3Line />,
-    }, {
-        voteIcon: <DiMongodb />,
-        title: "MongoDb",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <FaReact />,
-        title: "React Js",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <FaWordpress />,
-        title: "Wordpress",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <GiWireframeGlobe />,
-        title: "WireFrame",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <FiFigma />,
-        title: "Figma",
-        arrowIcon: <RiLoader3Line />,
-    },
+  {
+    voteIcon: <FaNodeJs />,
+    title: "Node Js",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <DiMongodb />,
+    title: "MongoDb",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <TbBrandNextjs />,
+    title: "Next Js",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <FaReact />,
+    title: "React Js",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <FaWordpress />,
+    title: "Wordpress",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <GiWireframeGlobe />,
+    title: "WireFrame",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <FiFigma />,
+    title: "Figma",
+    arrowIcon: <RiLoader3Line />,
+  },
 
-    {
-        voteIcon: <RiFlutterFill />,
-        title: "Flutter",
-        arrowIcon: <RiLoader3Line />,
-    },
+  {
+    voteIcon: <RiFlutterFill />,
+    title: "Flutter",
+    arrowIcon: <RiLoader3Line />,
+  },
 
-    {
-        voteIcon: <SiDart />,
-        title: "Dart",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <DiJavascript1 />,
-        title: "JavaScript",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <FaHtml5 />,
-        title: "HTML",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <FaCss3 />,
-        title: "CSS",
-        arrowIcon: <RiLoader3Line />,
-    },
+  {
+    voteIcon: <SiDart />,
+    title: "Dart",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <DiJavascript1 />,
+    title: "JavaScript",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <FaHtml5 />,
+    title: "HTML",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <FaCss3 />,
+    title: "CSS",
+    arrowIcon: <RiLoader3Line />,
+  },
 
-    {
-        voteIcon: <FaGithub />,
-        title: "Github",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <SiAdobefonts />,
-        title: "Adobe Fireworks",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <BsFileWord />,
-        title: "Microsoft Word",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <GiParachute />,
-        title: "Corel Draw",
-        arrowIcon: <RiLoader3Line />,
-    },
-    {
-        voteIcon: <FaFilePowerpoint />,
-        title: "Power Point",
-        arrowIcon: <RiLoader3Line />,
-    },
+  {
+    voteIcon: <FaGithub />,
+    title: "Github",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <SiAdobefonts />,
+    title: "Adobe Fireworks",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <BsFileWord />,
+    title: "Microsoft Word",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <GiParachute />,
+    title: "Corel Draw",
+    arrowIcon: <RiLoader3Line />,
+  },
+  {
+    voteIcon: <FaFilePowerpoint />,
+    title: "Power Point",
+    arrowIcon: <RiLoader3Line />,
+  },
 
-    // {
-    //     voteIcon: <SiHuawei />,
-    //     title: "Huawei Networking",
-    //     arrowIcon: <BiLoaderAlt />,
-    // },
-
-
-
-
-]
+  // {
+  //     voteIcon: <SiHuawei />,
+  //     title: "Huawei Networking",
+  //     arrowIcon: <BiLoaderAlt />,
+  // },
+];
