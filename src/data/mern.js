79e49122aeda0b811c509919bc483bdf039a65c1
abuXahra <abdulmuminisah@yahoo.com>
2024-10-11 09:49:05 +0100

    updated

--- src/data/mern.js
+++ src/data/mern.js
@@ -1,8 +1,16 @@
 import news from "../images/mern/news.PNG";
 import manzo from "../images/mern/manzo.PNG";
 import inventory from "../images/mern/inventory.PNG";
+import blogDashboard from "../images/mern/blogDashboard.PNG";
 
 export const mern = [
+  {
+    title: "News Blog Dashboard",
+    description: "Dashboard for a news blog Web App",
+    imgUrl: blogDashboard,
+    siteUrl: "https://blog-dasboard.onrender.com/",
+    status: "Online",
+  },
   {
     title: "News Blog",
     description: "It is a news blog Web App",
