e94ade21041c5d00042ad45e607eb55c53e6e781
abuXahra <abdulmuminisah@yahoo.com>
2025-04-08 11:47:56 +0100

    updated

--- src/data/mern.js
+++ src/data/mern.js
@@ -3,8 +3,24 @@ import manzo from "../images/mern/manzo.PNG";
 import inventory from "../images/mern/inventory.PNG";
 import blogDashboard from "../images/mern/blogDashboard.PNG";
 import formPicture from "../images/mern/form.PNG";
+import inventory2 from "../images/mern/ainventory.PNG";
+import inventory2 from "../images/mern/flight_tickecting.PNG";
 
 export const mern = [
+  {
+    title: "Manzo Travels  Web App",
+    description: "Travel and Tour Agency Ticketing Website",
+    imgUrl: manzo,
+    siteUrl: "https://manzotravels.com",
+    status: "Online",
+  },
+  {
+    title: "Inventory System",
+    description: "Inventory Management System for stock taking",
+    imgUrl: inventory2,
+    siteUrl: "https://inventory-management-system-xcvp.onrender.com/dashboard",
+    status: "Online",
+  },
   {
     title: "User Data Form",
     description: "Form for saving User into database",
@@ -24,13 +40,6 @@ export const mern = [
     description: "It is a news blog Web App",
     imgUrl: news,
     siteUrl: "https://newsblog-244u.onrender.com/",
-    status: "Online",
-  },
-  {
-    title: "Manzo Travels Website",
-    description: "Travel and Tour Agency Website",
-    imgUrl: manzo,
-    siteUrl: "https://travel-tour-sigma.vercel.app/",
     status: "Online",
   },
 
